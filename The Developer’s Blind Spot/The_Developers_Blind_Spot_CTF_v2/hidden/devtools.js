a5eb2b45c8971fe7ad6ec47184f0608a09f8d4ce
Thasya Sri Khandrika <145111710+Thasyasri@users.noreply.github.com>
2026-02-27 14:13:41 +0530

    Add files via upload

---
+++ The Developer’s Blind Spot/The_Developers_Blind_Spot_CTF_v2/hidden/devtools.js
@@ -0,0 +1,5 @@
+// Internal developer debugging variable
+var internal_key = "Umt4QlIzdGtaWFp6WDNOb2IzVnNaRjl1WlhabGNsOTBjblZ6ZEY5amIyMXRaVzUwYzMwPQ==";
+
+// Hint for those who think in layers:
+// atob(atob(internal_key))
